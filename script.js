700ea1473bec250622baa06fecd5ff065099dd7b
Raphael Radespiel <contatoraphael1701@gmail.com>
2022-07-11 20:39:52 -0300

    Add hability to play a round and get a winner

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let symbolMap = {
-  0: "rock",
-  1: "paper",
-  2: "scissors"
+  0: "Rock",
+  1: "Paper",
+  2: "Scissors"
 };
 
 function computerPlay(){
@@ -12,3 +12,35 @@ function humanPlay(){
   return prompt("Please write your move: ", "");
 }
 
+function playRound(playerSelection, computerSelection){
+
+  if(playerSelection.toUpperCase() === computerSelection.toUpperCase()){
+    return `It's a tie! ${computerSelection} is friends with ${computerSelection}`; 
+  }
+
+  switch(computerSelection){
+    case 'Rock':
+      if(playerSelection.toUpperCase() === 'PAPER'){
+        return "You win! Paper beats Rock.";
+      }
+      else{
+        return `You loose! ${computerSelection} beats ${playerSelection}`;
+      }
+    case 'Paper':
+      if(playerSelection.toUpperCase() === 'SCISSORS'){
+        return "You win! Scissors beats Paper.";
+      }
+      else{
+        return `You loose! ${computerSelection} beats ${playerSelection}`;
+      }
+    case 'Scissors':
+      if(playerSelection.toUpperCase() === 'ROCK'){
+        return "You win! Rock beats Scissors.";
+      }
+      else{
+        return `You loose! ${computerSelection} beats ${playerSelection}`;
+      }
+    default:
+      return computerSelection + ' ' + playerSelection;
+  }
+}
